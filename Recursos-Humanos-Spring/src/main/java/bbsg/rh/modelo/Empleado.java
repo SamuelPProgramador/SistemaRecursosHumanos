4e5acb210dbc412ec7f13e17c57ca2bbfb0a9ce9
Samuel <samuelariaspenalo.com>
2025-01-17 07:18:02 -0400

    he agregado react

--- Recursos-Humanos-Spring/src/main/java/bbsg/rh/modelo/Empleado.java
+++ Recursos-Humanos-Spring/src/main/java/bbsg/rh/modelo/Empleado.java
@@ -12,12 +12,65 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Getter
+@Setter
 public class Empleado {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer idEmpleado;
-    String  Nombre;
+    String  nombre;
     String apellido;
     String departamento;
     Double sueldo;
+
+    public Integer getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(Integer idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public Double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(Double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "idEmpleado=" + idEmpleado +
+                ", Nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", departamento='" + departamento + '\'' +
+                ", sueldo=" + sueldo +
+                '}';
+    }
 }
